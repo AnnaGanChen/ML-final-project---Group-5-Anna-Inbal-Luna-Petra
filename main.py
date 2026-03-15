3b784aa7ae65caa2961b8d05faafb8ee38fb28e4
Luna Gutiérrez <63022071+lunagutierrez@users.noreply.github.com>
2026-03-15 08:21:08 +0100

    Upload main for central running

---
+++ main.py
@@ -0,0 +1,18 @@
+from unsupervised import run_unsupervised
+from supervised import run_supervised
+from cnn import run_cnn
+
+
+def main():
+
+    print("Starting ML pipeline")
+
+    run_unsupervised()
+    run_supervised()
+    run_cnn()
+
+    print("Pipeline complete")
+
+
+if __name__ == "__main__":
+    main()
